a7dda9d47b847ca4d84bd7330ce7049213930143
ches- <Es18.03.88@gmail.com>
2021-04-04 00:25:30 +0300

    lesson5, this

--- src/lessons/lesson5/lesson5.ts
+++ src/lessons/lesson5/lesson5.ts
@@ -1,3 +1,5 @@
+import compileStreaming = WebAssembly.compileStreaming;
+
 console.log('Lesson 5');
 
 // Keyword - this
@@ -19,6 +21,7 @@ console.log('Lesson 5');
 // Call, Apply, Bind
 // https://learn.javascript.ru/call-apply-decorators
 // https://medium.com/@stasonmars/%D0%BF%D0%BE%D0%B4%D1%80%D0%BE%D0%B1%D0%BD%D0%BE-%D0%BE-%D0%BC%D0%B5%D1%82%D0%BE%D0%B4%D0%B0%D1%85-apply-call-%D0%B8-bind-%D0%BD%D0%B5%D0%BE%D0%B1%D1%85%D0%BE%D0%B4%D0%B8%D0%BC%D1%8B%D1%85-%D0%BA%D0%B0%D0%B6%D0%B4%D0%BE%D0%BC%D1%83-javascript-%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%87%D0%B8%D0%BA%D1%83-ddd5f9b06290
+
 
 
 // Task 01
